d2a1620f08f48fe67c85101f8fb71d2e4133cdce
duplobaustein <valentin.zopp@outlook.com>
2026-03-27 19:13:05 +0100

    Add files via upload

---
+++ Load Default7 Layout/Load_Layout_C.lua
@@ -0,0 +1,17 @@
+-- Set Track Layout C (TCP + MCP)
+-- Part of the "Set Track Layout (A, B, C)" package by duplobaustein
+
+local layout_name = "Layout C"
+
+reaper.PreventUIRefresh(1)
+
+local track_count = reaper.CountTracks(0)
+for i = 0, track_count - 1 do
+  local track = reaper.GetTrack(0, i)
+  reaper.GetSetMediaTrackInfo_String(track, "P_TCP_LAYOUT", layout_name, true)
+  reaper.GetSetMediaTrackInfo_String(track, "P_MCP_LAYOUT", layout_name, true)
+end
+
+reaper.PreventUIRefresh(-1)
+reaper.TrackList_AdjustWindows(false)
+reaper.UpdateArrange()
