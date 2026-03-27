c6d063c48775ab81a4e0429fea3a7e378bcab0d7
duplobaustein <valentin.zopp@outlook.com>
2026-03-27 19:12:42 +0100

    Create Load_Layout_A.lua

---
+++ Load Default7 Layout/Load_Layout_A.lua
@@ -0,0 +1,26 @@
+-- @description Set Track Layout (A, B, C)
+-- @author duplobaustein
+-- @version 1.0
+-- @repository https://github.com/duplobaustein/vazupReaperScripts/raw/main/index.xml
+-- @provides
+--   [main] Load_Layout_A.lua
+--   [main] Load_Layout_B.lua
+--   [main] Load_Layout_C.lua
+-- @about
+--   Sets TCP and MCP layout to A, B, or C for all tracks in the project.
+--   Compatible with the default Reaper theme.
+
+local layout_name = "Layout A"
+
+reaper.PreventUIRefresh(1)
+
+local track_count = reaper.CountTracks(0)
+for i = 0, track_count - 1 do
+  local track = reaper.GetTrack(0, i)
+  reaper.GetSetMediaTrackInfo_String(track, "P_TCP_LAYOUT", layout_name, true)
+  reaper.GetSetMediaTrackInfo_String(track, "P_MCP_LAYOUT", layout_name, true)
+end
+
+reaper.PreventUIRefresh(-1)
+reaper.TrackList_AdjustWindows(false)
+reaper.UpdateArrange()
